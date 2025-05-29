004f89f9b5e0659c471684c3fc554a1f1485d5a5
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2025-05-29 12:32:55 +0300

    QoL macros

--- efi.cpp
+++ efi.cpp
@@ -1,20 +1,29 @@
 #include <efi.h>
 #define efistr(x) (unsigned short*)(L ## x)
+#define println(x) ST->ConOut->OutputString(ST->ConOut, efistr(x L"\r\n"))
+#define wait_kb()																												\
+	{																																			\
+		EFI_INPUT_KEY Key;																									\
+		ST->ConIn->Reset(ST->ConIn, FALSE);																	\
+		while (ST->ConIn->ReadKeyStroke(ST->ConIn, &Key) == EFI_NOT_READY);	\
+	}
+#define stop(msg, code)													\
+	{																							\
+		println(msg);																\
+		println("\r\nPress any key to continue");		\
+		wait_kb();																	\
+		return code;																\
+	}
+#define eficheck(x, msg)												\
+	{																							\
+		EFI_STATUS Status = (x);										\
+		if (EFI_ERROR(Status)) stop(msg, Status);		\
+	}
 
 EFI_STATUS efi_main(EFI_HANDLE, EFI_SYSTEM_TABLE* ST)
 {
-  EFI_STATUS Status;
-  EFI_INPUT_KEY Key;
-
-  Status = ST->ConOut->OutputString(ST->ConOut, efistr("Hello World\r\n"));
-  if (EFI_ERROR(Status))
-    return Status;
-
-  Status = ST->ConIn->Reset(ST->ConIn, FALSE);
-  if (EFI_ERROR(Status))
-    return Status;
-
-  while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY);
-
-  return Status;
+	eficheck(println("Hello world"), "failed to print");
+	wait_kb();
+  
+	return 0;
 }
